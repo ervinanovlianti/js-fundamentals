3a7ae68f30a305f08a49d8067f4240ca3dbbd034
ervinanovlianti <ervinanov03@gmail.com>
2022-07-01 21:05:48 +0800

    object oriented programming

---
+++ oop_class-method.js
@@ -0,0 +1,4 @@
+// instantiate class sebelum menggunakan method yang ada dalam kelasnya.
+const mail1 = new Mail();
+// sendMessage = method dalam kelas Mail.
+mail1.sendMessage('hallo', 'penerima@dicoding.com');
---
+++ oop_private-atribute.js
@@ -0,0 +1,35 @@
+// mengubah attribute contacts pada class mail menjadi tidak bisa diakses di luar class atau oleh object yang menjadi instansiasi class tsb
+/** 
+cara 1, menggunakan var (hanya dapat digunakan pada penulisan kelas menggunakan statement `function`)
+**/
+// var contacts = [];
+// contoh
+function Mail() {
+    this.from = 'pengirim@dicoding.com';
+    var contacts = [];
+}
+
+/**
+cara 2, cara ini dapat digunakan pada penulisan kelas menggunakan statement `function` dan `class`
+**/
+// this._contacts = []
+// contoh
+class Mail {
+    constructor() {
+        this._contacts = [];
+        this.from = 'pengirim@dicoding.com';
+    }
+}
+
+/** 
+cara 3, menambahkan prefix # , cara ini dapat digunakan pada penulisan kelas menggunakan statement `class` saja 
+hanya saja cara ini belum sepenuhnya didukung oleh js engine versi lama
+  **/
+// #contacts = [];
+// contoh
+class Mail {
+    #contacts = [];
+    constructor() {
+        this.from = 'pengirim@dicoding.com';
+    }
+}
