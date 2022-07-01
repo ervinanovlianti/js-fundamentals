da88f74da767011b20bac94955398412a19b96a5
ervinanovlianti <ervinanov03@gmail.com>
2022-07-01 21:07:02 +0800

    recommit and rename file

--- class-method.js
+++
@@ -1,4 +0,0 @@
-// instantiate class sebelum menggunakan method yang ada dalam kelasnya.
-const mail1 = new Mail();
-// sendMessage = method dalam kelas Mail.
-mail1.sendMessage('hallo', 'penerima@dicoding.com');
--- static-method.js
+++
@@ -1,8 +0,0 @@
-// menambahkan sebuah method untuk memeriksa apakah sebuah input adalah nomor handphone:
-class Mail {
-    static isValidPhone(phone) {
-        return typeof phone === 'number';
-    }
-}
-// memanggil fungsi tersebut tanpa membuat instance kelas Mail terlebih dahulu.
-Mail.isValidPhone(089000000000) //true
